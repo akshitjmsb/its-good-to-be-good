590637bcb7e302bfbfa1b4b27fb1522499ed1cbd
Akshit Gupta <akshitgupta.jmsb@gmail.com>
2026-02-25 15:59:21 -0500

    Housekeeping: remove Storybook and Chromatic stack

--- src/components/react/BaseModal.tsx
+++ src/components/react/BaseModal.tsx
@@ -13,7 +13,7 @@ export const BaseModal: React.FC<BaseModalProps> = ({
   onClose, 
   title, 
   children, 
-  modalId = 'storybook-modal' 
+  modalId = 'app-modal' 
 }) => {
   if (!isOpen) return null;
 
